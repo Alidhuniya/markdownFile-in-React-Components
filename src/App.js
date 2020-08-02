f4e7395faffff5165bc75cad00660beb8effe97b
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-08-02 10:39:59 -0700

    formatted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 // import logo from './logo.svg';
 import AppMarkdown from "./readme/README.md";
-import './App.css';
-import marked from "marked"; 
-import ReactMarkdown from 'react-markdown';
+import "./App.css";
+import marked from "marked";
+import ReactMarkdown from "react-markdown";
 
 /*
 Note: Below are the example of rendering markdown file using marked and react-markdown package. marked package is commented because 
@@ -14,7 +14,6 @@ this article covers sanitizing topic(https://dev.to/jam3/how-to-prevent-xss-atta
 
 */
 
-
 // class App extends React.Component {  // this one is by using marked package
 
 //   constructor() {
@@ -24,7 +23,7 @@ this article covers sanitizing topic(https://dev.to/jam3/how-to-prevent-xss-atta
 
 //   componentDidMount() {
 //     const readmePath = require("./readme/README.md");
-  
+
 //     fetch(readmePath)
 //       .then(response => {
 //         return response.text()
@@ -36,10 +35,9 @@ this article covers sanitizing topic(https://dev.to/jam3/how-to-prevent-xss-atta
 //       })
 //   }
 
-  
 //   render() {
 //     const { markdown } = this.state;
-  
+
 //     return (
 //       <section>
 //         <article dangerouslySetInnerHTML={{__html: markdown}}></article>
@@ -49,25 +47,28 @@ this article covers sanitizing topic(https://dev.to/jam3/how-to-prevent-xss-atta
 // }
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { markdown: "" };
+	}
 
-  constructor(props) {
-    super(props);
-    this.state = { markdown: '' };
-   
-  }
+	componentWillMount() {
+		// Get the contents from the Markdown file and put them in the React state, so we can reference it in render() below.
+		fetch(AppMarkdown)
+			.then((res) => res.text())
+			.then((output) => this.setState({ markdown: output }));
+	}
 
-  componentWillMount() {
-    // Get the contents from the Markdown file and put them in the React state, so we can reference it in render() below.
-    fetch(AppMarkdown).then(res => res.text()).then(text => this.setState({ markdown: text }));
-  }
-
-
-  render() {
-    const { markdown } = this.state;
-    return <ReactMarkdown className="section" source={markdown} escapeHtml={false}/>;
-  }
+	render() {
+		const { markdown } = this.state;
+		return (
+			<ReactMarkdown
+				className="section"
+				source={markdown}
+				escapeHtml={false}
+			/>
+		);
+	}
 }
 
 export default App;
-
-
